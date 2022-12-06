a34c719dfd0ce99868d4099870cedafd59061b76
egorcouldlie <couldliegor@gmail.com>
2022-12-06 17:32:23 +0300

    2nd exercise

--- src/withMain/Main.java
+++ src/withMain/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
-    public static void task1(){
+
+    public static void task1() {
         //task number 1
         Scanner num = new Scanner(System.in);
         System.out.println("Enter number 0, or 1");
         byte clientOs = num.nextByte();
-        switch (clientOs){
+        switch (clientOs) {
             case 0:
                 System.out.println("Download version for iOS from this link: ");
                 break;
@@ -19,6 +21,43 @@ public class Main {
                 break;
             default:
                 System.out.println("Sorry, we haven't done a website for your OS yet!");
+        }
+    }
+
+    public static void task2() {
+        //task 2
+        Scanner word = new Scanner(System.in);
+        System.out.println("Enter number 0, or 1");
+        int clientOs = word.nextInt();
+
+        Scanner year = new Scanner(System.in);
+        System.out.println("Please, enter the year of your phone!");
+        int yearOfPhone = year.nextInt();
+        boolean earlyVersion = yearOfPhone < 2015;
+        boolean goodVersion = yearOfPhone >= 2015;
+
+        if (goodVersion) {
+            switch (clientOs) {
+                case 0:
+                    System.out.println("Download version for iOS from this link: ");
+                    break;
+                case 1:
+                    System.out.println("Download version for Android from this link: ");
+                    break;
+                default:
+                    System.out.println("Sorry, we haven't done a website for your OS yet!");
+            }
+        } else if (earlyVersion) {
+            switch (clientOs) {
+                case 0:
+                    System.out.println("Download light version from this link: ");
+                    break;
+                case 1:
+                    System.out.println("Download light version for Android from this link: ");
+                    break;
+                default:
+                    System.out.println("Sorry, we haven't done a website for your OS yet!");
+            }
         }
     }
 }
